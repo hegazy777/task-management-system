6844d655f0f4e1cbd0c768096dd3ebd02c344859
mohamed hegazy <hheegg9999@gmail.com>
2025-03-27 00:25:28 +0200

    routr add

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,51 @@
 
 import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import AuthLayout from './modules/Shard/AuthLayout/AuthLayout'
+import ForgetPass from './modules/Authentcations/ForgetPass/ForgetPass'
+import ChangePass from './modules/Authentcations/ChangePass/ChangePass'
+import VerifyAccount from './modules/Authentcations/VerifyAccount/VerifyAccount'
+import Register from './modules/Authentcations/Register/Register'
+import NotFound from './modules/NotFound/NotFound'
+import Login from './modules/Authentcations/Login/Login'
+
+
+
 
 function App() {
-  
+
+
+
+
+
+  const router = createBrowserRouter([
+    {
+      path:'',
+      element: <AuthLayout />,
+      children: [
+        { index: true, element: <Login /> },
+        { path: 'login', element: <Login /> },
+        { path: 'register', element: <Register /> },
+        { path: 'forgetPass', element: <ForgetPass/> },
+        { path: 'changePass', element: <ChangePass /> },
+        { path: 'verifyAccount', element: <VerifyAccount /> }
+      ]
+    },
+   
+    {
+      path: '*',  
+      element: <NotFound />
+    }
+  ])
 
   return (
-    <> 
-
+    <>
      
+      <RouterProvider router={router} ></RouterProvider>
     </>
+  
   )
+  
 }
 
 export default App
