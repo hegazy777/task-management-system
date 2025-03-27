8799fb57aad02ce5f94defa454dfc40cdb2b5ffc
HalimMahmoud <halimmahmoud50@yahoo.com>
2025-03-27 23:28:55 +0200

    context & login

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,42 @@
-
-import './App.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import AuthLayout from './modules/Shard/AuthLayout/AuthLayout'
-import ForgetPass from './modules/Authentcations/ForgetPass/ForgetPass'
-import ChangePass from './modules/Authentcations/ChangePass/ChangePass'
-import VerifyAccount from './modules/Authentcations/VerifyAccount/VerifyAccount'
-import Register from './modules/Authentcations/Register/Register'
-import NotFound from './modules/NotFound/NotFound'
-import Login from './modules/Authentcations/Login/Login'
-
-
-
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AuthLayout from "./modules/Shard/AuthLayout/AuthLayout";
+import ForgetPass from "./modules/Authentcations/ForgetPass/ForgetPass";
+import ChangePass from "./modules/Authentcations/ChangePass/ChangePass";
+import VerifyAccount from "./modules/Authentcations/VerifyAccount/VerifyAccount";
+import Register from "./modules/Authentcations/Register/Register";
+import NotFound from "./modules/NotFound/NotFound";
+import Login from "./modules/Authentcations/Login/Login";
+import AuthProvider from "./contexts/AuthContext";
+import { ToastContainer } from "react-toastify";
 
 function App() {
-
-
-
-
-
   const router = createBrowserRouter([
     {
-      path:'',
+      path: "",
       element: <AuthLayout />,
       children: [
         { index: true, element: <Login /> },
-        { path: 'login', element: <Login /> },
-        { path: 'register', element: <Register /> },
-        { path: 'forgetPass', element: <ForgetPass/> },
-        { path: 'changePass', element: <ChangePass /> },
-        { path: 'verifyAccount', element: <VerifyAccount /> }
-      ]
+        { path: "login", element: <Login /> },
+        { path: "register", element: <Register /> },
+        { path: "forgetPass", element: <ForgetPass /> },
+        { path: "changePass", element: <ChangePass /> },
+        { path: "verifyAccount", element: <VerifyAccount /> },
+      ],
     },
-   
+
     {
-      path: '*',  
-      element: <NotFound />
-    }
-  ])
+      path: "*",
+      element: <NotFound />,
+    },
+  ]);
 
   return (
-    <>
-     
-      <RouterProvider router={router} ></RouterProvider>
-    </>
-  
-  )
-  
+    <AuthProvider>
+      <RouterProvider router={router}></RouterProvider>
+      <ToastContainer />
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
