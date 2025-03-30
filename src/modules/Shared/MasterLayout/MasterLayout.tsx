c890b9a56496f7ed56a23badc16e396b086da902
mohamed hegazy <hheegg9999@gmail.com>
2025-03-31 01:08:02 +0200

    add header and navbare

--- src/modules/Shared/MasterLayout/MasterLayout.tsx
+++ src/modules/Shared/MasterLayout/MasterLayout.tsx
@@ -1,12 +1,24 @@
-import { Container } from "react-bootstrap";
-import { Outlet } from "react-router-dom";
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
-export default function MasterLayer() {
+import { Outlet } from "react-router-dom";
+import SideBar from "../SideBar/SideBar";
+import NavBar from "../NavBar/NavBar";
+
+const Layout = () => {
   return (
-    <div className="master-container">
-      <Container fluid className="">
-        <Outlet />
-      </Container>
-    </div>
+    <Container fluid>
+      <Row>
+        <Col xs={12} md={3} lg={2} className="bg-light vh-100 w-25 bg-warning">
+          <SideBar />
+        </Col>
+        <Col xs={12} md={9} lg={10} className="p-0 w-75">
+          <NavBar />
+          <Outlet />
+        </Col>
+      </Row>
+    </Container>
   );
-}
+};
+
+export default Layout;
