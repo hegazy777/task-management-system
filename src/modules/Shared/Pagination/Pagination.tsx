739ddd5a032b18ea031ebcc486162177ab06d4a2
HalimMahmoud <halimmahmoud50@yahoo.com>
2025-03-31 19:08:08 +0200

    fix bug if the records less than 10 records in pagination

--- src/modules/Shared/Pagination/Pagination.tsx
+++ src/modules/Shared/Pagination/Pagination.tsx
@@ -23,25 +23,44 @@ export default function Pagination({
 }) {
   const getPageSizes = (totalNumberOfRecords: number) =>
     Array.from(
-      { length: Math.floor(totalNumberOfRecords / 100) },
+      {
+        length: Math.floor(
+          totalNumberOfRecords < 100 ? 1 : totalNumberOfRecords / 100
+        ),
+      },
       (_, i) => (i + 1) * 10
     );
+
+  const Styles =
+    totalNumberOfRecords < 10
+      ? {
+          width: "35px",
+          backgroundImage: "none",
+          paddingRight: "10px",
+        }
+      : { width: "70px" };
+
   return (
     <Stack direction="horizontal" gap={3} className="justify-content-end">
       <Stack direction="horizontal" gap={3}>
         <div>Showing </div>
         <Form.Select
-          style={{ width: "70px" }}
+          disabled={totalNumberOfRecords < 10}
+          style={Styles}
           value={pageSize}
           onChange={(e: ChangeEvent<HTMLSelectElement>) =>
             setPageSize(Number(e.target.value))
           }
         >
-          {getPageSizes(totalNumberOfRecords).map((value, i) => (
-            <option key={i} value={value}>
-              {value}
-            </option>
-          ))}
+          {totalNumberOfRecords < 10 ? (
+            <option value={totalNumberOfRecords}>{totalNumberOfRecords}</option>
+          ) : (
+            getPageSizes(totalNumberOfRecords).map((value, i) => (
+              <option key={i} value={value}>
+                {value}
+              </option>
+            ))
+          )}
         </Form.Select>
         <div>{`of ${totalNumberOfRecords} Results`}</div>
       </Stack>
