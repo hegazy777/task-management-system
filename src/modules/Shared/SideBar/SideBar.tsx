5e0ee9b8bf53798c1a602faf0dda77cc85510764
HalimMahmoud <halimmahmoud50@yahoo.com>
2025-04-11 07:25:39 +0200

    hide new task component from sidebar

--- src/modules/Shared/SideBar/SideBar.tsx
+++ src/modules/Shared/SideBar/SideBar.tsx
@@ -69,9 +69,9 @@ export default function SideBar() {
             <FaTasks style={{ marginRight: "10px" }} /> Tasks
           </MenuItem>
 
-          <MenuItem component={<Link to="new-task" />}>
+          {/* <MenuItem component={<Link to="new-task" />}>
             <FaTasks style={{ marginRight: "10px" }} /> Tasks
-          </MenuItem>
+          </MenuItem> */}
           <MenuItem component={<Link to="TaskBoard" />}>
             <FaTasks style={{ marginRight: "10px" }} /> Task Board
           </MenuItem>
