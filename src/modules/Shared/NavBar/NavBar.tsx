3c59de26efb5de834d2a046f0d4d9275eed3aeb8
mohamed hegazy <hheegg9999@gmail.com>
2025-04-11 18:39:52 +0200

    add dark mode

--- src/modules/Shared/NavBar/NavBar.tsx
+++ src/modules/Shared/NavBar/NavBar.tsx
@@ -1,13 +1,25 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Navbar, Container, Nav, Image, Dropdown } from "react-bootstrap";
 import { FaBell } from "react-icons/fa";
 import navLogo from "../../../assets/navLogo.png";
 import Ellipse from "../../../assets/Ellipse.png";
 import { AuthContext } from "../../../contexts/AuthContext";
 import { imageURL } from "../../../services/api/apiConfig";
+import DarkMode from "../DarkMode/DarkMode";
 
 export default function NavBar() {
   const { user, logout } = useContext(AuthContext);
+  const [isDark, setIsDark] = useState(false);
+  const handleDarkModeChange = () => {
+    setIsDark(!isDark);
+  };
+  useEffect(() => {
+    if (isDark) {
+      document.body.classList.add("dark-mode");
+    } else {
+      document.body.classList.remove("dark-mode");
+    }
+  }, [isDark]);
   return (
     <Navbar bg="white" expand="lg" className="shadow-sm px-3">
       <Container
@@ -23,9 +35,11 @@ export default function NavBar() {
           />
         </Navbar.Brand>
 
-        <Nav className="d-flex align-items-center">
+        <Nav className="d-flex align-items-center al">
+        
+          <DarkMode handleChange={handleDarkModeChange} isChecked={isDark} />
+       
           <FaBell className="text-warning fs-4 me-4 cursor-pointer" />
-
           <Dropdown align="end">
             <Dropdown.Toggle
               variant="white"
@@ -38,10 +52,10 @@ export default function NavBar() {
                 height={40}
                 className="me-2"
               />
-              <div className="text-start d-none d-md-block">
+              <div className="text-start d-none d-md-block user">
                 <strong>{user?.userName}</strong>
                 <br />
-                <small className="text-muted">{user?.email}</small>
+                <small className=" user">{user?.email}</small>
               </div>
             </Dropdown.Toggle>
             <Dropdown.Menu>
